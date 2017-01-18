f6077aedea37b326a4a98e5a55aa749f143b5763
joliu <joliu@s-an.org>
2017-01-18 23:20:22 +0800

    多进程新的监听任务列表并执行的主程序

---
+++ matlabServer/threadser.py
@@ -0,0 +1,31 @@
+import ListenSer
+import time
+
+from threading import Thread
+
+class RunThread(Thread):
+    """
+    多进程运行任务
+    """
+    
+    def __init__(self,li):
+        """初始化类"""
+        Thread.__init__(self)
+        self.list = li
+
+    def run(self):
+        ListenSer.run_listen(self.list)
+        print(time.asctime)
+
+if __name__ == '__main__':
+    while True:
+        #:输入每次循环从列表中取多少组任务
+        lists = ListenSer.gettask(4)
+        if len(lists) == 0:
+            time.sleep(6)
+            print(time.asctime()+': sleep')
+        else:
+            for li in lists:
+                my_thread = RunThread(li)
+                my_thread.start()
+        
