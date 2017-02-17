347ee86c8fbc3c327b4282fc08bec16f6865b59f
joliu <joliu@s-an.org>
2017-02-17 22:10:38 +0800

    上传任务到数据库

---
+++ matlabServer/upload_task.py
@@ -0,0 +1,25 @@
+import MySQLdb
+import time
+
+def save_task(user_email,filename):
+    '''保存数据到服务器'''
+    start_time = time.asctime()
+    task=filename
+    value = [user_email,task,start_time]
+    try:
+        conn = MySQLdb.connect(host='112.74.171.161',user='root',passwd='WyrXa9')
+        cur = conn.cursor()
+        conn.select_db('task_db')
+        cur.execute("insert into task_list(user_email,task,start_time) values (%s,%s,%s)",value)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except MySQLdb as e:
+        print("Mysql Error%d:%s" % (e.args[0],e.args[1]))
+
+
+if __name__ == '__main__':
+    user_email = 'joliu@s-an.org'
+    task = 'out.py'
+    save_task(user_email,task)
+
