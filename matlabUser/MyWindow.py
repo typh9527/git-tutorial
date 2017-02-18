17b53d3358c53897ecb0e4bce942ddf5ac74b22b
joliu <joliu@s-an.org>
2017-02-18 19:38:03 +0800

    优化界面

--- matlabUser/MyWindow.py
+++ matlabUser/MyWindow.py
@@ -8,7 +8,9 @@ class MyWindow(QtWidgets.QWidget):
         super(MyWindow,self).__init__()
         self.myButton = QtWidgets.QPushButton(self)
         self.myButton.setObjectName("myButton")
-        self.myButton.setText("Test")
+        self.myButton.setText("上传")
+        self.myButton.move(260,100)
+        self.setGeometry(300, 380, 360, 170)
         self.myButton.clicked.connect(self.msg)
 
     def msg(self):
@@ -17,6 +19,7 @@ class MyWindow(QtWidgets.QWidget):
         host = config.get("Settings","host")
         user_email = config.get("Settings","email")
         print(user_email)
+        flag = "failed"
         fileName1, filetype = QFileDialog.getOpenFileName(self,
                                     "选取文件",
                                     "/",
@@ -55,7 +58,12 @@ class MyWindow(QtWidgets.QWidget):
 #                                    "All Files (*);;Text Files (*.txt)")
 
 if __name__=="__main__":  
-    app=QtWidgets.QApplication(sys.argv)  
+    app=QtWidgets.QApplication(sys.argv)	
     myshow=MyWindow()
+    label = QtWidgets.QLabel(myshow)
+    label_text = "<br>使用说明：</br><br>1、使用前请打开log.ini文件，配置登陆信息</br><br>2、点击上传按钮选择zip文件</br>"
+    label2_text = "<br>注意：</br><br>zip文件要求：<br/><br>1、压缩包(即项目名)名字不要重复</br><br>2、zip文件内必须有和zip文件名相同的子目录<br/><br>3、子目录中的要执行的.m文件必须以main命名</br><br>具体请参考附带的test\
+                   .zip文件</br>"
+    label.setText(label_text+label2_text)
     myshow.show()
     sys.exit(app.exec_())  
